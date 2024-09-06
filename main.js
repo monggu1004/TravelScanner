ee5c4d2e586dde894033112279c8156682f2ec00
monggu1004 <7243527@naver.com>
2024-09-06 14:40:25 +0900

    .
    .

--- main.js
+++ main.js
@@ -9,4 +9,13 @@ function openmenu() {
   menubox.classList.toggle("hidden");
 }
 
+async function api() {
+  const data = await fetch(
+    "https://apis.data.go.kr/B551011/KorService1/locationBasedList1?serviceKey=2IH5PyYaWE%2FpKFRNC0FqAUfRHJj091EvFJGNV7wdtf35vQq0QILwUk7jFssn%2BR0DaSnXlMC55OE8mZtbRorVSA%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A&mapX=126.981611&mapY=37.568477&radius=1000&contentTypeId=15"
+  );
+  console.log(data);
+  const date2 = await data.json();
+  console.log(date2);
+}
+api();
 bars.addEventListener("click", openmenu);
