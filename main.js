00fda5f23f7be1c14de1aebb1a499dd436f3e73a
monggu1004 <7243527@naver.com>
2024-09-10 17:39:30 +0900

    api

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const bars = document.querySelector(".fa-bars");
 const header = document.querySelector("header");
 const menubox = document.createElement("div");
+const boton = document.querySelector("boton");
 menubox.classList.add("menubox");
 header.appendChild(menubox);
 menubox.classList.add("hidden");
@@ -8,23 +9,26 @@ menubox.classList.add("hidden");
 function openmenu() {
   menubox.classList.toggle("hidden");
 }
-function runOk(position) {
-  console.log(position);
+
+const API_KEY = "0cb7bf6dd6a2cd3aa583cb6e16fd0525";
+async function runOk(position) {
   const lati = position.coords.latitude;
   const longi = position.coords.longitude;
-  const myapi = "0cb7bf6dd6a2cd3aa583cb6e16fd0525";
-
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=lati&lon=longi&appid=myapi`;
-  console.log(url);
+  console.log(lati);
+  console.log(longi);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${longi}&appid=${API_KEY}`;
+  const dd = await fetch(url).then((response) => response.json());
+  console.log(dd.weather[0].main);
 }
 function runError() {
   alert("에러");
 }
 navigator.geolocation.getCurrentPosition(runOk, runError);
-
+const box = document.querySelector(".dataBox");
+box.addEventListener("click", runOk);
 bars.addEventListener("click", openmenu);
 
-// async function api() {
+// async function api() {r
 //   const data = await fetch(
 //   );
 //   console.log(data);
